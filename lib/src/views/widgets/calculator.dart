754be32bbe525cbd73b99c54b7656c4e0aee6cbe
Hugo Bock <h.bock@easyrecrue.com>
2020-06-27 14:31:35 +0000

    base done

--- lib/src/views/widgets/calculator.dart
+++ lib/src/views/widgets/calculator.dart
@@ -13,7 +13,7 @@ class _CalculatorState extends State<Calculator> {
   Widget build(BuildContext context) {
     return Container(
       child: GridView.count(
-        crossAxisCount: 4,
+        crossAxisCount: 3,
         children: _buttons(),
       ),
     );
@@ -21,10 +21,24 @@ class _CalculatorState extends State<Calculator> {
 
   List<Widget> _buttons() {
     var buttons = List<Widget>();
-    buttons.add(_button(
-      'C',
-      () => _reset,
-    ));
+    // buttons.add(_emptyButton());
+    // buttons.add(_emptyButton());
+    // buttons.add(_resetButton());
+    // buttons.add(_divisionButton());
+    buttons.add(_numberButton(7));
+    buttons.add(_numberButton(8));
+    buttons.add(_numberButton(9));
+    // buttons.add(_multiplicationButton());
+    buttons.add(_numberButton(4));
+    buttons.add(_numberButton(5));
+    buttons.add(_numberButton(6));
+    // buttons.add(_substractionButton());
+    buttons.add(_numberButton(1));
+    buttons.add(_numberButton(2));
+    buttons.add(_numberButton(3));
+    buttons.add(_numberButton(0));
+    buttons.add(_additionButton());
+    buttons.add(_resetButton());
     return buttons;
   }
 
@@ -36,11 +50,48 @@ class _CalculatorState extends State<Calculator> {
     return GestureDetector(
       onTap: onTap,
       child: Container(
-        color: backgroundColor,
-        child: Text(text),
+        decoration: BoxDecoration(
+          color: backgroundColor,
+          border: Border.all(
+            color: Colors.black.withOpacity(0.3),
+            width: 1,
+          ),
+        ),
+        child: Center(child: Text(text)),
       ),
     );
   }
 
+  // all buttons
+
+  // Reset button
+  Widget _resetButton() => _button(
+        'C',
+        () => _reset,
+      );
   void _reset() => null;
+
+  // Empty button
+  Widget _emptyButton() => _button('', () => null);
+
+  // Division button
+  Widget _divisionButton() => _button('/', () => _divide);
+  void _divide() => null;
+
+  // Multiplication button
+  Widget _multiplicationButton() => _button('X', () => _multiply);
+  void _multiply() => null;
+
+  // Add button
+  Widget _additionButton() => _button('+', () => _add);
+  void _add() => null;
+
+  // Substraction button
+  Widget _substractionButton() => _button('-', () => _substract);
+  void _substract() => null;
+
+  // Number button
+  Widget _numberButton(int number) =>
+      _button(number.toString(), () => _numberClicked(number));
+  void _numberClicked(int number) => null;
 }
